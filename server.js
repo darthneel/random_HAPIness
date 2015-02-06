2da037b981adaff6fd7e5ef19d381d5a230b3a7b
neelp22 <neelp22@gmail.com>
2015-02-06 12:16:01 -0500

    added server basics. testing params usage

---
+++ server.js
@@ -0,0 +1,41 @@
+var Hapi = require('hapi');
+var request = require('request');
+var Good = require('good');
+
+var server = new Hapi.Server();
+server.connection({port: 3000});
+
+
+server.route({
+	method: "GET",
+	path: "/",
+	handler: function(req, res){
+		res("Yo breh");
+	}
+});
+
+server.route({
+	method: "GET",
+	path: '/{name}',
+	handler: function(req, res){
+		res('Yo ' + req.params.name);
+	}
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args:[{ log: '*', response: '*' }]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+	server.start(function(){
+		console.log("server started")
+	});
+});
